5b32a0e6c0ec702844e25e10f9d29dd7b2c0b13e
rovshen <rovsen.dev@gmail.com>
2024-06-17 01:42:37 +0400

    Коррекция параллакса

--- src/Components/reusableComponents/parallaxSection/GetScreenConfig.js
+++ src/Components/reusableComponents/parallaxSection/GetScreenConfig.js
@@ -3,9 +3,9 @@ export const getScreenConfig = (width, height) => {
     { minWidth: 1440, minHeight: 900, speed: 0.2, offset: 100, backgroundSize: 'cover' },
     { minWidth: 1280, minHeight: 800, speed: 0.2, offset: 120, backgroundSize: 'cover' },
     { minWidth: 1024, minHeight: 768, speed: 0.2, offset: 100, backgroundSize: 'cover' },
-    { minWidth: 1024, minHeight: 600, speed: 0.2, offset: 90, backgroundSize: 'cover' },
+    { minWidth: 1024, minHeight: 600, speed: 0.1, offset: -30, backgroundSize:'cover'},
 
-    { minWidth: 1024, minHeight: 1366, speed: 0.2, offset: 0, backgroundSize: 'cover' },
+    // { minWidth: 1024, minHeight: 1366, speed: 0.2, offset: 0, backgroundSize: 'cover' },
 
     { minWidth: 912, minHeight: 1368, speed: 0.2, offset: 170, backgroundSize: 'cover' },
     { minWidth: 853, minHeight: 1280, speed: 0.2, offset: 180, backgroundSize: 'cover' },
@@ -15,10 +15,14 @@ export const getScreenConfig = (width, height) => {
     { minWidth: 576, minHeight: 400, speed: 0.2, offset: 100, backgroundSize: 'cover' },
     { minWidth: 540, minHeight: 720, speed: 0.2, offset: 110, backgroundSize: 'cover' },
     { minWidth: 430, minHeight: 932, speed: 0.2, offset: 110, backgroundSize: 'cover' },
-    { minWidth: 412, minHeight: 914, speed: 0.2, offset: 120, backgroundSize: 'cover' },
+
+    { minWidth: 414, minHeight: 896, speed: 0.2, offset: -90, backgroundSize: '430%' },
+
     { minWidth: 390, minHeight: 844, speed: 0.2, offset: 100, backgroundSize: 'cover' },
     { minWidth: 360, minHeight: 740, speed: 0.2, offset: 100, backgroundSize: 'cover' },
-    { minWidth: 375, minHeight: 667, speed: 0.2, offset: 80, backgroundSize: 'cover' },
+
+    { minWidth: 375, minHeight: 667, speed: 0.2, offset: -80, backgroundSize: '350%'}, //  <--- ГОТОВ
+
     { minWidth: 344, minHeight: 882, speed: 0.2, offset: 100, backgroundSize: '350%' },
     // Добавляем другие конфигурации по мере необходимости
   ];
