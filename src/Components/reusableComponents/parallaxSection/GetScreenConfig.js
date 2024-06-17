b5c9fc7c8422c2ac66c2705955c7a8ada88a0590
rovshen <rovsen.dev@gmail.com>
2024-06-17 21:00:17 +0400

    Настройка параллакса

--- src/Components/reusableComponents/parallaxSection/GetScreenConfig.js
+++ src/Components/reusableComponents/parallaxSection/GetScreenConfig.js
@@ -1,35 +1,37 @@
 export const getScreenConfig = (width, height) => {
   const screenConfig = [
-    { minWidth: 1440, minHeight: 900, speed: 0.2, offset: 100, backgroundSize: 'cover' },
-    { minWidth: 1280, minHeight: 800, speed: 0.2, offset: 120, backgroundSize: 'cover' },
-    { minWidth: 1024, minHeight: 768, speed: 0.2, offset: 100, backgroundSize: 'cover' },
-    { minWidth: 1024, minHeight: 600, speed: 0.1, offset: -30, backgroundSize:'cover'},
+    { minWidth: 1920,  speed: 0.2, offset: -90, backgroundSize: '120%'},
+    { minWidth: 1280, speed: 0.15, offset: -70, backgroundSize: '130%' }, //  <====================
 
-    // { minWidth: 1024, minHeight: 1366, speed: 0.2, offset: 0, backgroundSize: 'cover' },
-
-    { minWidth: 912, minHeight: 1368, speed: 0.2, offset: 170, backgroundSize: 'cover' },
-    { minWidth: 853, minHeight: 1280, speed: 0.2, offset: 180, backgroundSize: 'cover' },
-    { minWidth: 820, minHeight: 1180, speed: 0.2, offset: 130, backgroundSize: 'cover' },
-    { minWidth: 768, minHeight: 1024, speed: 0.2, offset: 100, backgroundSize: '250%' },
+    { minWidth: 1440, minHeight: 900, speed: 0.15, offset: 185, backgroundSize: '100%' },    // <======= 15
+    { minWidth: 1280, minHeight: 800, speed: 0.15, offset: 120, backgroundSize: '100%' },    // <======= 14
+    { minWidth: 1024, minHeight: 1366, speed: 0.15, offset: -110, backgroundSize: '250%' },   // <======= 9
+    { minWidth: 912, minHeight: 1368, speed: 0.12, offset: -110, backgroundSize: '260%' },    // <======= 10
+    { minWidth: 853, minHeight: 1280, speed: 0.15, offset: -100, backgroundSize: '280%' },    // <======= 13
+    { minWidth: 820, minHeight: 1180, speed: 0.15, offset: -90, backgroundSize: '265%' },    // <======= 8
+    { minWidth: 768, minHeight: 1024, speed: 0.15, offset: -105, backgroundSize: '250%' },  // <======= 7
     { minWidth: 768, minHeight: 600, speed: 0.2, offset: 100, backgroundSize: 'cover' },
     { minWidth: 576, minHeight: 400, speed: 0.2, offset: 100, backgroundSize: 'cover' },
-    { minWidth: 540, minHeight: 720, speed: 0.2, offset: 110, backgroundSize: 'cover' },
-    { minWidth: 430, minHeight: 932, speed: 0.2, offset: 110, backgroundSize: 'cover' },
-
-    { minWidth: 414, minHeight: 896, speed: 0.2, offset: -90, backgroundSize: '430%' },
-
-    { minWidth: 390, minHeight: 844, speed: 0.2, offset: 100, backgroundSize: 'cover' },
-    { minWidth: 360, minHeight: 740, speed: 0.2, offset: 100, backgroundSize: 'cover' },
-
-    { minWidth: 375, minHeight: 667, speed: 0.2, offset: -80, backgroundSize: '350%'}, //  <--- ГОТОВ
-
-    { minWidth: 344, minHeight: 882, speed: 0.2, offset: 100, backgroundSize: '350%' },
-    // Добавляем другие конфигурации по мере необходимости
+    { minWidth: 540, minHeight: 720, speed: 0.14, offset: -55, backgroundSize: '240%' },   // <======= 11
+    { minWidth: 430, minHeight: 932, speed: 0.15, offset: -75, backgroundSize: '390%' },    // <======= 4
+    { minWidth: 412, minHeight: 915, speed: 0.12, offset: -55, backgroundSize: '380%' },   // <======= 3
+    { minWidth: 414, minHeight: 896, speed: 0.12, offset: -60, backgroundSize: '370%' },   // <======= 5
+    { minWidth: 390, minHeight: 844, speed: 0.1, offset: -60, backgroundSize: '365%' },  // <======= 1
+    { minWidth: 375, minHeight: 667, speed: 0.12, offset: -40, backgroundSize: '305%' },   // <======= 2
+    { minWidth: 360, minHeight: 740, speed: 0.13, offset: -60, backgroundSize: '360%' },   // <======= 6
+    { minWidth: 344, minHeight: 882, speed: 0.08, offset: -47, backgroundSize: '420%' },    // <======= 12
+    // Другие конфигурации...
   ];
 
-  const config = screenConfig.find(
-    (config) => width >= config.minWidth && height >= config.minHeight
-  );
-  console.log("Screen config:", config); // Лог для проверки параметров конфигурации
+  // Находим конфигурацию, которая подходит для текущего размера экрана
+  const config = screenConfig.find(config => width >= config.minWidth);
+
+  // Возвращаем найденную конфигурацию или последнюю, если не нашли подходящую
   return config || screenConfig[screenConfig.length - 1];
 };
+
+
+
+
+
+
--- src/Components/reusableComponents/parallaxSection/Parallax.jsx
+++ src/Components/reusableComponents/parallaxSection/Parallax.jsx
@@ -1,39 +1,6 @@
-import React, { useEffect, useRef, useState } from "react";
-import "./parallax.scss";
-import { getScreenConfig } from "./GetScreenConfig";
-
-// export const getScreenConfig = (width, height) => {
-//   const screenConfig = [
-//     { minWidth: 1440, minHeight: 900, speed: 0.2, offset: 100 },
-//     { minWidth: 1280, minHeight: 800, speed: 0.2, offset: 120 },
-//     { minWidth: 1024, minHeight: 768, speed: 0.2, offset: 100 },
-//     { minWidth: 1024, minHeight: 600, speed: 0.2, offset: 90 },
-
-//     { minWidth: 1024, minHeight: 1366, speed: 0.2, offset: 120 },
-
-//     { minWidth: 912, minHeight: 1368, speed: 0.2, offset: 170 },
-//     { minWidth: 853, minHeight: 1280, speed: 0.2, offset: 180 },
-//     { minWidth: 820, minHeight: 1180, speed: 0.2, offset: 130 },
-//     { minWidth: 768, minHeight: 1024, speed: 0.2, offset: 100 },
-//     { minWidth: 768, minHeight: 600, speed: 0.2, offset: 100 },
-//     { minWidth: 576, minHeight: 400, speed: 0.2, offset: 100 },
-//     { minWidth: 540, minHeight: 720, speed: 0.2, offset: 110 },
-//     { minWidth: 430, minHeight: 932, speed: 0.2, offset: 110 },
-//     { minWidth: 412, minHeight: 914, speed: 0.2, offset: 120 },
-//     { minWidth: 390, minHeight: 844, speed: 0.2, offset: 100 },
-//     { minWidth: 360, minHeight: 740, speed: 0.2, offset: 100 },
-//     { minWidth: 375, minHeight: 667, speed: 0.2, offset: 80 },
-//     { minWidth: 344, minHeight: 882, speed: 0.2, offset: 100 },
-
-//     // Добавляем другие конфигурации по мере необходимости
-//   ];
-
-//   const config = screenConfig.find(
-//     (config) => width >= config.minWidth && height >= config.minHeight
-//   );
-//   console.log("Screen config:", config); // Лог для проверки параметров конфигурации
-//   return config || screenConfig[screenConfig.length - 1];
-// };
+// import React, { useEffect, useRef, useState } from "react";
+// import "./parallax.scss";
+// import { getScreenConfig } from "./GetScreenConfig";
 
 // function Parallax({
 //   backgroundImage,
@@ -63,8 +30,11 @@ import { getScreenConfig } from "./GetScreenConfig";
 //         offset,
 //       } = getScreenConfig(windowWidth, windowHeight);
 
-//       const backgroundPosY = (rect.top - windowHeight / 2) * speed + offset;
-//       console.log("backgroundPosY:", backgroundPosY); // Лог для проверки значения backgroundPosY
+//       let backgroundPosY = 0;
+
+//       if (speed && offset) {
+//         backgroundPosY = (rect.top - windowHeight / 2) * speed + offset;
+//       }
 
 //       setBackgroundPosition(`center ${backgroundPosY}px`);
 //       setBackgroundSize(bgSize);
@@ -122,6 +92,17 @@ import { getScreenConfig } from "./GetScreenConfig";
 
 // export default Parallax;
 
+
+
+
+
+
+
+
+import React, { useEffect, useRef, useState } from "react";
+import "./parallax.scss";
+import { getScreenConfig } from "./GetScreenConfig";
+
 function Parallax({
   backgroundImage,
   title,
@@ -150,8 +131,11 @@ function Parallax({
         offset,
       } = getScreenConfig(windowWidth, windowHeight);
 
-      const backgroundPosY = (rect.top - windowHeight / 2) * speed + offset;
-      console.log("backgroundPosY:", backgroundPosY); // Лог для проверки значения backgroundPosY
+      let backgroundPosY = 0;
+
+      if (speed && offset) {
+        backgroundPosY = (rect.top - windowHeight / 2) * speed + offset;
+      }
 
       setBackgroundPosition(`center ${backgroundPosY}px`);
       setBackgroundSize(bgSize);
