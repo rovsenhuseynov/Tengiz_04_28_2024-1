107fe262aa970aec0d40825dc91a15360e9143a6
rovshen <rovsen.dev@gmail.com>
2024-06-18 19:53:04 +0400

    Добавка разрешения 1080х1920

--- src/Components/reusableComponents/parallaxSection/GetScreenConfig.js
+++ src/Components/reusableComponents/parallaxSection/GetScreenConfig.js
@@ -1,25 +1,563 @@
+// export const getScreenConfig = (width, height) => {
+//   const screenConfig = [
+//     {
+//       minWidth: 1920,
+//       maxWidth: Infinity,
+//       minHeight: 0,
+//       maxHeight: Infinity,
+//       speed: 0.2,
+//       offset: -350,
+//       backgroundSize: "120%",
+//     },
+//     {
+//       minWidth: 1280,
+//       maxWidth: 1919,
+//       minHeight: 0,
+//       maxHeight: Infinity,
+//       speed: 0.17,
+//       offset: -80,
+//       backgroundSize: "135%",
+//     },
+
+//     // Условия для экранов шириной 1440 пикселей и выше
+//     {
+//       minWidth: 1440,
+//       maxWidth: 1919,
+//       minHeight: 900,
+//       maxHeight: Infinity,
+//       speed: 0.15,
+//       offset: 185,
+//       backgroundSize: "100%",
+//     },
+
+//     // Условия для экранов шириной 1280 пикселей и выше
+//     {
+//       minWidth: 1280,
+//       maxWidth: 1439,
+//       minHeight: 800,
+//       maxHeight: Infinity,
+//       speed: 0.15,
+//       offset: 120,
+//       backgroundSize: "100%",
+//     },
+
+//     // Условия для экранов шириной 1024 пикселя и высотой от 1366 пикселей
+//     {
+//       minWidth: 1024,
+//       maxWidth: 1279,
+//       minHeight: 1366,
+//       maxHeight: Infinity,
+//       speed: 0.15,
+//       offset: -100,
+//       backgroundSize: "245%",
+//     },
+
+//     // Условия для экранов шириной 1024 пикселя и высотой до 1365 пикселей
+//     {
+//       minWidth: 1024,
+//       maxWidth: 1279,
+//       minHeight: 600,
+//       maxHeight: 1365,
+//       speed: 0.15,
+//       offset: -100,
+//       backgroundSize: "120%",
+//     },
+
+//     // Остальные конфигурации
+//     {
+//       minWidth: 912,
+//       maxWidth: 1023,
+//       minHeight: 1368,
+//       maxHeight: Infinity,
+//       speed: 0.12,
+//       offset: -105,
+//       backgroundSize: "260%",
+//     },
+//     {
+//       minWidth: 853,
+//       maxWidth: 911,
+//       minHeight: 1280,
+//       maxHeight: Infinity,
+//       speed: 0.15,
+//       offset: -100,
+//       backgroundSize: "280%",
+//     },
+//     {
+//       minWidth: 820,
+//       maxWidth: 852,
+//       minHeight: 1180,
+//       maxHeight: Infinity,
+//       speed: 0.15,
+//       offset: -90,
+//       backgroundSize: "265%",
+//     },
+//     {
+//       minWidth: 768,
+//       maxWidth: 819,
+//       minHeight: 1024,
+//       maxHeight: Infinity,
+//       speed: 0.15,
+//       offset: -105,
+//       backgroundSize: "250%",
+//     },
+//     {
+//       minWidth: 540,
+//       maxWidth: 767,
+//       minHeight: 720,
+//       maxHeight: Infinity,
+//       speed: 0.14,
+//       offset: -55,
+//       backgroundSize: "240%",
+//     },
+//     {
+//       minWidth: 430,
+//       maxWidth: 539,
+//       minHeight: 932,
+//       maxHeight: Infinity,
+//       speed: 0.15,
+//       offset: -75,
+//       backgroundSize: "390%",
+//     },
+//     {
+//       minWidth: 412,
+//       maxWidth: 429,
+//       minHeight: 915,
+//       maxHeight: Infinity,
+//       speed: 0.12,
+//       offset: -55,
+//       backgroundSize: "380%",
+//     },
+//     {
+//       minWidth: 414,
+//       maxWidth: 411,
+//       minHeight: 896,
+//       maxHeight: Infinity,
+//       speed: 0.12,
+//       offset: -60,
+//       backgroundSize: "370%",
+//     },
+//     {
+//       minWidth: 390,
+//       maxWidth: 413,
+//       minHeight: 844,
+//       maxHeight: Infinity,
+//       speed: 0.1,
+//       offset: -60,
+//       backgroundSize: "365%",
+//     },
+//     {
+//       minWidth: 375,
+//       maxWidth: 389,
+//       minHeight: 667,
+//       maxHeight: Infinity,
+//       speed: 0.12,
+//       offset: -40,
+//       backgroundSize: "305%",
+//     },
+//     {
+//       minWidth: 360,
+//       maxWidth: 374,
+//       minHeight: 740,
+//       maxHeight: Infinity,
+//       speed: 0.1,
+//       offset: -38,
+//       backgroundSize: "340%",
+//     },
+//     {
+//       minWidth: 344,
+//       maxWidth: 359,
+//       minHeight: 882,
+//       maxHeight: Infinity,
+//       speed: 0.1,
+//       offset: -47,
+//       backgroundSize: "420%",
+//     },
+//   ];
+
+//   // Находим конфигурацию, которая подходит для текущего размера экрана
+//   const config = screenConfig.find((config) => {
+//     return (
+//       width >= config.minWidth &&
+//       width <= config.maxWidth &&
+//       height >= config.minHeight &&
+//       height <= config.maxHeight
+//     );
+//   });
+
+//   // Возвращаем найденную конфигурацию или последнюю, если не нашли подходящую
+//   return config || screenConfig[screenConfig.length - 1];
+// };
+
+
+
+
+
+// export const getScreenConfig = (width, height) => {
+//   const screenConfig = [
+//     { minWidth: 1920, maxWidth: Infinity, minHeight: 0, maxHeight: Infinity, speed: 0.2, offset: -350, backgroundSize: "120%" },
+//     { minWidth: 1280, maxWidth: 1919, minHeight: 0, maxHeight: Infinity, speed: 0.17, offset: -75, backgroundSize: "135%" },
+
+//     // Условия для экранов шириной 1440 пикселей и выше
+//     {
+//       minWidth: 1440,
+//       maxWidth: 1919,
+//       minHeight: 900,
+//       maxHeight: Infinity,
+//       speed: 0.15,
+//       offset: 185,
+//       backgroundSize: "100%",
+//     },
+
+//     // Условия для экранов шириной 1280 пикселей и выше
+//     {
+//       minWidth: 1280,
+//       maxWidth: 1439,
+//       minHeight: 800,
+//       maxHeight: Infinity,
+//       speed: 0.15,
+//       offset: 120,
+//       backgroundSize: "100%",
+//     },
+
+//     // Условия для экранов шириной 1024 пикселя и высотой от 1366 пикселей
+//     {
+//       minWidth: 1024,
+//       maxWidth: 1279,
+//       minHeight: 1366,
+//       maxHeight: Infinity,
+//       speed: 0.15,
+//       offset: -100,
+//       backgroundSize: "245%",
+//     },
+
+//     // Условия для экранов шириной 1024 пикселя и высотой до 1365 пикселей
+//     {
+//       minWidth: 1024,
+//       maxWidth: 1279,
+//       minHeight: 600,
+//       maxHeight: 1365,
+//       speed: 0.15,
+//       offset: -100,
+//       backgroundSize: "120%",
+//     },
+
+//     // Условия для экранов шириной 1080 пикселей и высотой 1920 пикселей
+//     {
+//       minWidth: 1080,
+//       maxWidth: 1080,
+//       minHeight: 1920,
+//       maxHeight: 1920,
+//       speed: 0.15,
+//       offset: -50,
+//       backgroundSize: "250%",
+//     },
+
+//     // Остальные конфигурации
+//     {
+//       minWidth: 912,
+//       maxWidth: 1023,
+//       minHeight: 1368,
+//       maxHeight: Infinity,
+//       speed: 0.12,
+//       offset: -105,
+//       backgroundSize: "260%",
+//     },
+//     {
+//       minWidth: 853,
+//       maxWidth: 911,
+//       minHeight: 1280,
+//       maxHeight: Infinity,
+//       speed: 0.15,
+//       offset: -100,
+//       backgroundSize: "280%",
+//     },
+//     {
+//       minWidth: 820,
+//       maxWidth: 852,
+//       minHeight: 1180,
+//       maxHeight: Infinity,
+//       speed: 0.15,
+//       offset: -90,
+//       backgroundSize: "265%",
+//     },
+//     {
+//       minWidth: 768,
+//       maxWidth: 819,
+//       minHeight: 1024,
+//       maxHeight: Infinity,
+//       speed: 0.15,
+//       offset: -105,
+//       backgroundSize: "250%",
+//     },
+//     {
+//       minWidth: 540,
+//       maxWidth: 767,
+//       minHeight: 720,
+//       maxHeight: Infinity,
+//       speed: 0.14,
+//       offset: -55,
+//       backgroundSize: "240%",
+//     },
+//     {
+//       minWidth: 430,
+//       maxWidth: 539,
+//       minHeight: 932,
+//       maxHeight: Infinity,
+//       speed: 0.15,
+//       offset: -75,
+//       backgroundSize: "390%",
+//     },
+//     {
+//       minWidth: 412,
+//       maxWidth: 429,
+//       minHeight: 915,
+//       maxHeight: Infinity,
+//       speed: 0.12,
+//       offset: -55,
+//       backgroundSize: "380%",
+//     },
+//     {
+//       minWidth: 414,
+//       maxWidth: 411,
+//       minHeight: 896,
+//       maxHeight: Infinity,
+//       speed: 0.12,
+//       offset: -60,
+//       backgroundSize: "370%",
+//     },
+//     {
+//       minWidth: 390,
+//       maxWidth: 413,
+//       minHeight: 844,
+//       maxHeight: Infinity,
+//       speed: 0.1,
+//       offset: -60,
+//       backgroundSize: "365%",
+//     },
+//     {
+//       minWidth: 375,
+//       maxWidth: 389,
+//       minHeight: 667,
+//       maxHeight: Infinity,
+//       speed: 0.12,
+//       offset: -40,
+//       backgroundSize: "305%",
+//     },
+//     {
+//       minWidth: 360,
+//       maxWidth: 374,
+//       minHeight: 740,
+//       maxHeight: Infinity,
+//       speed: 0.1,
+//       offset: -38,
+//       backgroundSize: "340%",
+//     },
+//     {
+//       minWidth: 344,
+//       maxWidth: 359,
+//       minHeight: 882,
+//       maxHeight: Infinity,
+//       speed: 0.1,
+//       offset: -47,
+//       backgroundSize: "420%",
+//     },
+//   ];
+
+//   // Находим конфигурацию, которая подходит для текущего размера экрана
+//   const config = screenConfig.find((config) => {
+//     return width >= config.minWidth && width <= config.maxWidth && height >= config.minHeight && height <= config.maxHeight;
+//   });
+
+//   // Возвращаем найденную конфигурацию или последнюю, если не нашли подходящую
+//   return config || screenConfig[screenConfig.length - 1];
+// };
+
+
+
+
+
+// export const getScreenConfig = (width, height) => {
+//   const screenConfig = [
+//     { minWidth: 1920, maxWidth: Infinity, minHeight: 0, maxHeight: Infinity, speed: 0.2, offset: -350, backgroundSize: "120%" },
+//     { minWidth: 1280, maxWidth: 1919, minHeight: 0, maxHeight: Infinity, speed: 0.17, offset: -75, backgroundSize: "135%" },
+    
+//     {
+//       minWidth: 1440,
+//       maxWidth: 1919,
+//       minHeight: 900,
+//       maxHeight: Infinity,
+//       speed: 0.15,
+//       offset: 185,
+//       backgroundSize: "100%",
+//     },
+//     {
+//       minWidth: 1280,
+//       maxWidth: 1439,
+//       minHeight: 800,
+//       maxHeight: Infinity,
+//       speed: 0.15,
+//       offset: 120,
+//       backgroundSize: "100%",
+//     },
+//     {
+//       minWidth: 1024,
+//       maxWidth: 1279,
+//       minHeight: 1366,
+//       maxHeight: Infinity,
+//       speed: 0.15,
+//       offset: -100,
+//       backgroundSize: "245%",
+//     },
+//     {
+//       minWidth: 1024,
+//       maxWidth: 1279,
+//       minHeight: 600,
+//       maxHeight: 1365,
+//       speed: 0.15,
+//       offset: -100,
+//       backgroundSize: "120%",
+//     },
+//     {
+//       minWidth: 1080,
+//       maxWidth: 1080,
+//       minHeight: 1920,
+//       maxHeight: 1920,
+//       speed: 0.15,
+//       offset: -0,
+//       backgroundSize: "250%",
+//     },
+//     {
+//       minWidth: 912,
+//       maxWidth: 1023,
+//       minHeight: 1368,
+//       maxHeight: Infinity,
+//       speed: 0.12,
+//       offset: -105,
+//       backgroundSize: "260%",
+//     },
+//     {
+//       minWidth: 853,
+//       maxWidth: 911,
+//       minHeight: 1280,
+//       maxHeight: Infinity,
+//       speed: 0.15,
+//       offset: -100,
+//       backgroundSize: "280%",
+//     },
+//     {
+//       minWidth: 820,
+//       maxWidth: 852,
+//       minHeight: 1180,
+//       maxHeight: Infinity,
+//       speed: 0.15,
+//       offset: -90,
+//       backgroundSize: "265%",
+//     },
+//     {
+//       minWidth: 768,
+//       maxWidth: 819,
+//       minHeight: 1024,
+//       maxHeight: Infinity,
+//       speed: 0.15,
+//       offset: -105,
+//       backgroundSize: "250%",
+//     },
+//     {
+//       minWidth: 540,
+//       maxWidth: 767,
+//       minHeight: 720,
+//       maxHeight: Infinity,
+//       speed: 0.14,
+//       offset: -55,
+//       backgroundSize: "240%",
+//     },
+//     {
+//       minWidth: 430,
+//       maxWidth: 539,
+//       minHeight: 932,
+//       maxHeight: Infinity,
+//       speed: 0.15,
+//       offset: -75,
+//       backgroundSize: "390%",
+//     },
+//     {
+//       minWidth: 412,
+//       maxWidth: 429,
+//       minHeight: 915,
+//       maxHeight: Infinity,
+//       speed: 0.12,
+//       offset: -55,
+//       backgroundSize: "380%",
+//     },
+//     {
+//       minWidth: 414,
+//       maxWidth: 411,
+//       minHeight: 896,
+//       maxHeight: Infinity,
+//       speed: 0.12,
+//       offset: -60,
+//       backgroundSize: "370%",
+//     },
+//     {
+//       minWidth: 390,
+//       maxWidth: 413,
+//       minHeight: 844,
+//       maxHeight: Infinity,
+//       speed: 0.1,
+//       offset: -60,
+//       backgroundSize: "365%",
+//     },
+//     {
+//       minWidth: 375,
+//       maxWidth: 389,
+//       minHeight: 667,
+//       maxHeight: Infinity,
+//       speed: 0.12,
+//       offset: -40,
+//       backgroundSize: "305%",
+//     },
+//     {
+//       minWidth: 360,
+//       maxWidth: 374,
+//       minHeight: 740,
+//       maxHeight: Infinity,
+//       speed: 0.1,
+//       offset: -38,
+//       backgroundSize: "340%",
+//     },
+//     {
+//       minWidth: 344,
+//       maxWidth: 359,
+//       minHeight: 882,
+//       maxHeight: Infinity,
+//       speed: 0.1,
+//       offset: -47,
+//       backgroundSize: "420%",
+//     },
+//   ];
+
+//   // Находим конфигурацию, которая подходит для текущего размера экрана
+//   const config = screenConfig.find((config) => {
+//     return width >= config.minWidth && width <= config.maxWidth && height >= config.minHeight && height <= config.maxHeight;
+//   });
+
+//   // Выводим конфигурацию в консоль для отладки
+//   console.log("Screen width:", width, "Screen height:", height);
+//   console.log("Selected configuration:", config);
+
+//   // Возвращаем найденную конфигурацию или последнюю, если не нашли подходящую
+//   return config || screenConfig[screenConfig.length - 1];
+// };
+
+
+
+
+
+
 export const getScreenConfig = (width, height) => {
   const screenConfig = [
-    {
-      minWidth: 1920,
-      maxWidth: Infinity,
-      minHeight: 0,
-      maxHeight: Infinity,
-      speed: 0.2,
-      offset: -350,
-      backgroundSize: "120%",
-    },
-    {
-      minWidth: 1280,
-      maxWidth: 1919,
-      minHeight: 0,
-      maxHeight: Infinity,
-      speed: 0.17,
-      offset: -80,
-      backgroundSize: "135%",
-    },
-
-    // Условия для экранов шириной 1440 пикселей и выше
+    { minWidth: 1920, maxWidth: Infinity, minHeight: 0, maxHeight: Infinity, speed: 0.2, offset: -350, backgroundSize: "120%" },
+    { minWidth: 1280, maxWidth: 1919, minHeight: 0, maxHeight: Infinity, speed: 0.17, offset: -75, backgroundSize: "135%" },
     {
       minWidth: 1440,
       maxWidth: 1919,
@@ -29,8 +567,6 @@ export const getScreenConfig = (width, height) => {
       offset: 185,
       backgroundSize: "100%",
     },
-
-    // Условия для экранов шириной 1280 пикселей и выше
     {
       minWidth: 1280,
       maxWidth: 1439,
@@ -40,8 +576,15 @@ export const getScreenConfig = (width, height) => {
       offset: 120,
       backgroundSize: "100%",
     },
-
-    // Условия для экранов шириной 1024 пикселя и высотой от 1366 пикселей
+    {
+      minWidth: 1080,
+      maxWidth: 1080,
+      minHeight: 1920,
+      maxHeight: 1920,
+      speed: 0.15,
+      offset: -150,
+      backgroundSize: "320%",
+    },
     {
       minWidth: 1024,
       maxWidth: 1279,
@@ -51,8 +594,6 @@ export const getScreenConfig = (width, height) => {
       offset: -100,
       backgroundSize: "245%",
     },
-
-    // Условия для экранов шириной 1024 пикселя и высотой до 1365 пикселей
     {
       minWidth: 1024,
       maxWidth: 1279,
@@ -62,26 +603,6 @@ export const getScreenConfig = (width, height) => {
       offset: -100,
       backgroundSize: "120%",
     },
-
-    
-   
-
-    // Условия для экранов шириной 1080 пикселей и высотой 1920 пикселей
-    {
-      minWidth: 1080,
-      maxWidth: 1080,
-      minHeight: 1920,
-      maxHeight: 1920,
-      speed: 0.15,
-      offset: -50,
-      backgroundSize: "150%",
-    },
-
-
-
-
-
-    // Остальные конфигурации
     {
       minWidth: 912,
       maxWidth: 1023,
@@ -201,6 +722,10 @@ export const getScreenConfig = (width, height) => {
       height <= config.maxHeight
     );
   });
+
+  // Выводим конфигурацию в консоль для отладки
+  console.log("Screen width:", width, "Screen height:", height);
+  console.log("Selected configuration:", config);
 
   // Возвращаем найденную конфигурацию или последнюю, если не нашли подходящую
   return config || screenConfig[screenConfig.length - 1];
--- src/Components/reusableComponents/parallaxSection/Parallax.jsx
+++ src/Components/reusableComponents/parallaxSection/Parallax.jsx
@@ -1,3 +1,207 @@
+// import React, { useEffect, useRef, useState } from "react";
+// import "./parallax.scss";
+// import { getScreenConfig } from "./GetScreenConfig";
+
+// function Parallax({
+//   backgroundImage,
+//   title,
+//   titleItalic,
+//   className,
+//   className_title,
+//   className_subtitle,
+//   isLargeScreen, // принимаем prop для определения пользовательских стилей
+// }) {
+//   const parallaxRef = useRef(null);
+//   const [scrollOpacity, setScrollOpacity] = useState(1);
+//   const [backgroundPosition, setBackgroundPosition] = useState("center center");
+//   const [backgroundSize, setBackgroundSize] = useState("cover");
+
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       const parallaxElement = parallaxRef.current;
+//       if (!parallaxElement) return;
+
+//       const rect = parallaxElement.getBoundingClientRect();
+//       const windowHeight = window.innerHeight;
+//       const windowWidth = window.innerWidth;
+
+//       const {
+//         speed,
+//         backgroundSize: bgSize,
+//         offset,
+//       } = getScreenConfig(windowWidth, windowHeight);
+
+//       let backgroundPosY = 0;
+
+//       if (speed && offset) {
+//         backgroundPosY = (rect.top - windowHeight / 2) * speed + offset;
+//       }
+
+//       setBackgroundPosition(`center ${backgroundPosY}px`);
+//       setBackgroundSize(bgSize);
+
+//       const distanceFromCenter = Math.abs(
+//         windowHeight / 2 - (rect.top + rect.height / 2)
+//       );
+//       const maxOpacityDistance = windowHeight / 2;
+//       const opacity = 1 - distanceFromCenter / maxOpacityDistance;
+//       setScrollOpacity(opacity < 0 ? 0 : opacity);
+//     };
+
+//     const handleResize = () => {
+//       handleScroll();
+//     };
+
+//     handleScroll();
+//     window.addEventListener("scroll", handleScroll);
+//     window.addEventListener("resize", handleResize);
+
+//     return () => {
+//       window.removeEventListener("scroll", handleScroll);
+//       window.removeEventListener("resize", handleResize);
+//     };
+//   }, []);
+
+//   const customStyles = isLargeScreen
+//     ? {
+//         backgroundSize: "175%", // переопределяем размер фона
+//         backgroundPosition: "top", // можно добавить другие стили
+//       }
+//     : {};
+
+//   return (
+//     <div
+//       className={`parallax-wrapper ${className}`}
+//       style={{
+//         backgroundImage: `url(${backgroundImage})`,
+//         backgroundPosition: backgroundPosition,
+//         backgroundSize: backgroundSize,
+//         ...customStyles, // применяем дополнительные стили
+//       }}
+//       ref={parallaxRef}
+//     >
+//       <div className="parallax__content">
+//         <h2
+//           className={`parallax__title-italics ${className_title}`}
+//           style={{ opacity: scrollOpacity }}
+//         >
+//           {titleItalic}
+//         </h2>
+//         <h2
+//           className={`parallax__title ${className_subtitle}`}
+//           style={{ opacity: scrollOpacity }}
+//         >
+//           {title}
+//         </h2>
+//       </div>
+//       <div className="mask"></div>
+//     </div>
+//   );
+// }
+
+// export default Parallax;
+
+
+
+
+
+// import React, { useEffect, useRef, useState } from "react";
+// import "./parallax.scss";
+// import { getScreenConfig } from "./GetScreenConfig";
+
+// function Parallax({
+//   backgroundImage,
+//   title,
+//   titleItalic,
+//   className,
+//   className_title,
+//   className_subtitle,
+// }) {
+//   const parallaxRef = useRef(null);
+//   const [scrollOpacity, setScrollOpacity] = useState(1);
+//   const [backgroundPosition, setBackgroundPosition] = useState("center center");
+//   const [backgroundSize, setBackgroundSize] = useState("cover");
+
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       const parallaxElement = parallaxRef.current;
+//       if (!parallaxElement) return;
+
+//       const rect = parallaxElement.getBoundingClientRect();
+//       const windowHeight = window.innerHeight;
+//       const windowWidth = window.innerWidth;
+
+//       const {
+//         speed,
+//         backgroundSize: bgSize,
+//         offset,
+//       } = getScreenConfig(windowWidth, windowHeight);
+
+//       let backgroundPosY = 0;
+
+//       if (speed && offset) {
+//         backgroundPosY = (rect.top - windowHeight / 2) * speed + offset;
+//       }
+
+//       setBackgroundPosition(`center ${backgroundPosY}px`);
+//       setBackgroundSize(bgSize);
+
+//       const distanceFromCenter = Math.abs(
+//         windowHeight / 2 - (rect.top + rect.height / 2)
+//       );
+//       const maxOpacityDistance = windowHeight / 2;
+//       const opacity = 1 - distanceFromCenter / maxOpacityDistance;
+//       setScrollOpacity(opacity < 0 ? 0 : opacity);
+//     };
+
+//     const handleResize = () => {
+//       handleScroll();
+//     };
+
+//     handleScroll();
+//     window.addEventListener("scroll", handleScroll);
+//     window.addEventListener("resize", handleResize);
+
+//     return () => {
+//       window.removeEventListener("scroll", handleScroll);
+//       window.removeEventListener("resize", handleResize);
+//     };
+//   }, []);
+
+//   return (
+//     <div
+//       className={`parallax-wrapper ${className}`}
+//       style={{
+//         backgroundImage: `url(${backgroundImage})`,
+//         backgroundPosition: backgroundPosition,
+//         backgroundSize: backgroundSize,
+//       }}
+//       ref={parallaxRef}
+//     >
+//       <div className="parallax__content">
+//         <h2
+//           className={`parallax__title-italics ${className_title}`}
+//           style={{ opacity: scrollOpacity }}
+//         >
+//           {titleItalic}
+//         </h2>
+//         <h2
+//           className={`parallax__title ${className_subtitle}`}
+//           style={{ opacity: scrollOpacity }}
+//         >
+//           {title}
+//         </h2>
+//       </div>
+//       <div className="mask"></div>
+//     </div>
+//   );
+// }
+
+// export default Parallax;
+
+
+
+
 import React, { useEffect, useRef, useState } from "react";
 import "./parallax.scss";
 import { getScreenConfig } from "./GetScreenConfig";
@@ -9,7 +213,6 @@ function Parallax({
   className,
   className_title,
   className_subtitle,
-  isLargeScreen, // принимаем prop для определения пользовательских стилей
 }) {
   const parallaxRef = useRef(null);
   const [scrollOpacity, setScrollOpacity] = useState(1);
@@ -62,13 +265,6 @@ function Parallax({
     };
   }, []);
 
-  const customStyles = isLargeScreen
-    ? {
-        backgroundSize: "175%", // переопределяем размер фона
-        backgroundPosition: "top", // можно добавить другие стили
-      }
-    : {};
-
   return (
     <div
       className={`parallax-wrapper ${className}`}
@@ -76,7 +272,6 @@ function Parallax({
         backgroundImage: `url(${backgroundImage})`,
         backgroundPosition: backgroundPosition,
         backgroundSize: backgroundSize,
-        ...customStyles, // применяем дополнительные стили
       }}
       ref={parallaxRef}
     >
