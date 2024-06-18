b615336556a6e1831e510d3ba8c2ca5b28ee8e00
rovshen <rovsen.dev@gmail.com>
2024-06-18 21:39:55 +0400

    П

--- src/pages/home/ParallaxesInHome/ParallaxElementFirst.jsx
+++ src/pages/home/ParallaxesInHome/ParallaxElementFirst.jsx
@@ -35,57 +35,16 @@
 
 // export default ParallaxElementFirst;
 
-// import React, { useEffect, useState } from "react";
-// import Parallax from "../../../Components/reusableComponents/parallaxSection/Parallax";
-// import bgImage from "../../../assets/image/0_66/R.webp";
-
-// function ParallaxElementFirst() {
-//   const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth > 1279);
-
-//   useEffect(() => {
-//     const handleResize = () => {
-//       setIsLargeScreen(window.innerWidth > 1279);
-//     };
-
-//     window.addEventListener("resize", handleResize);
-
-//     // Initial check
-//     handleResize();
-
-//     return () => {
-//       window.removeEventListener("resize", handleResize);
-//     };
-//   }, []);
-
-//   const customStyles = isLargeScreen
-//     ? {
-//         backgroundSize: "100%", // переопределяем размер фона
-
-//       }
-//     : {};
-
-//   return (
-//     <Parallax
-//       backgroundImage={bgImage}
-//       titleItalic="Tasteful"
-//       title="Recipes"
-//       customStyles={customStyles} // передаем пользовательские стили при больших экранах
-//     />
-//   );
-// }
-
-// export default ParallaxElementFirst;
-
 import React, { useEffect, useState } from "react";
 import Parallax from "../../../Components/reusableComponents/parallaxSection/Parallax";
 import bgImage from "../../../assets/image/0_66/R.webp";
 
 function ParallaxElementFirst() {
-  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth > 1280);
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth > 1279);
 
   useEffect(() => {
     const handleResize = () => {
-      setIsLargeScreen(window.innerWidth > 1280);
+      setIsLargeScreen(window.innerWidth > 1279);
     };
 
     window.addEventListener("resize", handleResize);
@@ -98,14 +57,58 @@ function ParallaxElementFirst() {
     };
   }, []);
 
+  const customStyles = isLargeScreen
+    ? {
+        backgroundSize: "100%", // переопределяем размер фона
+
+      }
+    : {};
+
   return (
     <Parallax
       backgroundImage={bgImage}
       titleItalic="Tasteful"
       title="Recipes"
-      isLargeScreen={isLargeScreen} // передаем состояние isLargeScreen
+      customStyles={customStyles} // передаем пользовательские стили при больших экранах
     />
   );
 }
 
 export default ParallaxElementFirst;
+
+
+
+
+// import React, { useEffect, useState } from "react";
+// import Parallax from "../../../Components/reusableComponents/parallaxSection/Parallax";
+// import bgImage from "../../../assets/image/0_66/R.webp";
+
+// function ParallaxElementFirst() {
+//   const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth > 1280);
+
+//   useEffect(() => {
+//     const handleResize = () => {
+//       setIsLargeScreen(window.innerWidth > 1280);
+//     };
+
+//     window.addEventListener("resize", handleResize);
+
+//     // Initial check
+//     handleResize();
+
+//     return () => {
+//       window.removeEventListener("resize", handleResize);
+//     };
+//   }, []);
+
+//   return (
+//     <Parallax
+//       backgroundImage={bgImage}
+//       titleItalic="Tasteful"
+//       title="Recipes"
+//       isLargeScreen={isLargeScreen} // передаем состояние isLargeScreen
+//     />
+//   );
+// }
+
+// export default ParallaxElementFirst;
